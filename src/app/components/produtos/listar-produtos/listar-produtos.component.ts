5f04a3e6d7683cd94f01f62f4ba0d7eb81bddd94
aamekst <mieko.alana@gmail.com>
2022-05-31 16:25:20 -0300

    [Table]

--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -7,7 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarProdutosComponent implements OnInit {
 
-  constructor() { }
+  listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
+  listaNumber: number [] = [15, 15, 18, 100];
+
+  objetoModelo = {
+    nome: 'Drake',
+    idade: 17,
+    altura: 1.80,
+    graduacao: true
+  };
+
+  constructor() {
+    for (let item of this.listaStrings)
+    {
+      console.log(item);
+    }
+
+    for (const item of this.listaNumber)
+    {
+      console.log(item);
+    }
+
+
+ 
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.nome);
+
+
+
+
+   }
 
   ngOnInit(): void {
   }
