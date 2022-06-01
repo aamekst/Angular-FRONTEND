1ec48b0a4f64ab254253cbb10ae67c69c94e7982
aamekst <mieko.alana@gmail.com>
2022-06-01 13:31:55 -0300

    [Listagem com array e ngFor]

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,10 +12,54 @@ export class HomeComponent implements OnInit {
 
    anuncio: string = `O ${this.nomeProduto} está em promoção!`;
    idProduto: number = 3;
-   precoProduto: number = 2.51;
+   precoProduto: number = 10.99;
    promocao: boolean = true;
    foto: string = 'assets/img/drake-e-josh.jpg';
    dataValidade = '2021-12-31'
+
+   listaStrings: string [] = ['Primeiro', 'Segundo', 'Terceiro'];
+   listaNumeros: number [] = [15, 29, 30, 50];
+
+   listaProdutos: any [] = [
+   {
+      nome: 'Curso de MySql',
+      precoProduto: 40,
+      validade: '2023-05-12',
+      id:1
+   },
+
+   {
+    nome: 'Curso de PHP',
+    precoProduto: 14.99,
+    validade: '2022-09-26',
+    id:2
+ },
+
+  {
+    nome: 'Curso de SQL SERVER',
+    precoProduto: 25,
+    validade: '2022-10-05',
+    id:3
+  },
+
+  {
+    nome: 'Curso de Design gráfico',
+    precoProduto: 40.99,
+    validade: '2023-12-20',
+    id:4
+  },
+
+  {
+    nome: 'Curso de Java',
+    precoProduto: 10,
+    validade: '2022-11-10',
+    id:5
+  },
+
+
+
+
+  ];
 
 
   constructor() {
@@ -28,16 +72,24 @@ export class HomeComponent implements OnInit {
     console.log('Preço:', this.precoProduto);
     console.log('Promoção:', this.promocao);
 
+    for(let item of this.listaStrings)
+    {
+      console.log(item);
+    }
 
-
-
-
-
-
+    for(const item of this.listaNumeros)
+    {
+      console.log(item);
+    }
 
   }
 
   ngOnInit(): void {
+  }
+  salvar(): void {
+    alert('Salvo com sucesso!');
+
+
   }
 
 }
