c49ad5a886893988f4ae52226ecb86b7db2f5c55
aamekst <mieko.alana@gmail.com>
2022-06-07 22:36:19 -0300

    [erro]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,6 +14,7 @@ import { LOCALE_ID } from '@angular/core';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -31,7 +33,9 @@ registerLocaleData(localePt);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule,
+    HttpClientModule
 
   ],
   providers: [
--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -1,3 +1,4 @@
+import { ProdutosService } from './../../../services/produtos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,44 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarProdutosComponent implements OnInit {
 
-  listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
-  listaNumber: number [] = [15, 15, 18, 100];
 
-  objetoModelo = {
-    nome: 'Drake',
-    idade: 17,
-    altura: 1.80,
-    graduacao: true
-  };
+  listaproduto: IProduto[] = [];
 
-  listaproduto: any [] = [
-    {nome: 'Curso de Angular', precoProduto: 35.56, validade: '2021-12-31', id: 1},
-    {nome: 'Curso de Ionic', precoProduto: 50, validade: '2021-12-31', id: 2},
-    {nome: 'Curso de Ionic Avançado', precoProduto: 80, validade: '2021-12-31', id: 3},
-  ];
-
-  constructor() {
-    for (let item of this.listaStrings)
-    {
-      console.log(item);
-    }
-
-    for (const item of this.listaNumber)
-    {
-      console.log(item);
-    }
-
-
-
-    console.log(this.objetoModelo);
-    console.log(this.objetoModelo.nome);
-
-
-
+  constructor(private ProdutosService: ProdutosService) {
 
    }
 
   ngOnInit(): void {
+    this.carregarProdutos();
+  }
+
+  carregarProdutos(): void {
+    this.ProdutosService.buscarTodos().subscribe(retorno =>{
+        this.listaproduto = retorno;
+    })
   }
 
 }
